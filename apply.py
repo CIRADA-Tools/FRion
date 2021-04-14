ca449e6693fe61846b1ea88f012bbc5fd371ae96
Cameron Van Eck <cameron.van.eck@dunlap.utoronto.ca>
2021-04-14 15:01:09 -0400

    Initial prototype of apply completed.

--- apply.py
+++ apply.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Tools for applying ionospheric Faraday rotation corrections to images and
-image cubes.
+image cubes. This file contains the functions needed to apply the correction
+to data cubes (either in memory, or in FITS files.)
 """
 
-
+import numpy as np
+from astropy.io import fits as pf
 
 
 
@@ -13,19 +15,176 @@ image cubes.
 
 
 def read_correction(filename):
-    pass
-    return correction_dictionary
+    (frequencies,real,imag)=np.genfromtxt(filename,unpack=True)
+    correction=real+1.j*imag
+    return frequencies, correction
 
+
+def find_freq_axis(header):
+    """Finds the frequency axis in a FITS header.
+    Input: header: a Pyfits header object.
+    Returns the axis number (as recorded in the FITS file, **NOT** in numpy ordering.)
+    Returns 0 if the frequency axis cannot be found.
+    """
+    freq_axis=0 #Default for 'frequency axis not identified'
+    #Check for frequency axes. Because I don't know what different formatting
+    #I might get ('FREQ' vs 'OBSFREQ' vs 'Freq' vs 'Frequency'), convert to
+    #all caps and check for 'FREQ' anywhere in the axis name.
+    for i in range(1,header['NAXIS']+1):  #Check each axis in turn.
+        try:
+            if 'FREQ' in header['CTYPE'+str(i)].upper():
+                freq_axis=i
+        except:
+            pass #The try statement is needed for if the FITS header does not
+                 # have CTYPE keywords.
+    return freq_axis
 
 
 def readData(Qfilename,Ufilename):
+    """
+    Open the Stokes Q and U input cubes (from the supplied 
+    file names) and return data-access variables and the header. 
+    Axes are re-ordered so that frequency is first, beyond that the number
+    and ordering of axes doesn't matter.
+    Uses the memmap functionality so that data isn't read into data; variables
+    are just handles to access the data on disk.
+    Returns the header from the Q file, the U file's header is ignored.
+    """    
     
+    hdulistQ=pf.open(Qfilename,memmap=True)
+    header=hdulistQ[0].header
+    Qdata=hdulistQ[0].data
+    hdulistU=pf.open(Ufilename,memmap=True)
+    Udata=hdulistU[0].data
+    
+    
+    N_dim=header['NAXIS'] #Get number of axes
+
+    freq_axis=find_freq_axis(header) 
+    #If the frequency axis isn't the last one, rotate the array until it is.
+    #Recall that pyfits reverses the axis ordering, so we want frequency on
+    #axis 0 of the numpy array.
+    if freq_axis != 0 and freq_axis != N_dim:
+        Qdata=np.moveaxis(Qdata,N_dim-freq_axis,0)
+        Udata=np.moveaxis(Udata,N_dim-freq_axis,0)
+
     
     return Qdata, Udata, header
 
 
-def write_corrected_cubes(Qoutputname,Qdata,Udata,header):
-    pass
+def write_corrected_cubes(Qoutputname,Uoutputname,Qcorr,Ucorr,header,overwrite=False):
+    """
+    Write the corrected Q and U data to FITS files. Copies the supplied header,
+    adding a note to the history saying that the correction was applied.
+    """
+    output_header=header.copy()
+    output_header.add_history('Corrected for ionospheric Faraday rotation using ionosphereFR_correct.')
+
+    #Get data back to original axis order, if necessary.
+    N_dim=output_header['NAXIS'] #Get number of axes
+    freq_axis=find_freq_axis(output_header)
+    if freq_axis != 0:
+        Qcorr=np.moveaxis(Qcorr,0,N_dim-freq_axis)
+        Ucorr=np.moveaxis(Ucorr,0,N_dim-freq_axis)
+
+
+    pf.writeto(Qoutputname,Qcorr,output_header,overwrite=overwrite)
+    pf.writeto(Qoutputname,Qcorr,output_header,overwrite=overwrite)
+
+    
+
+
+def correct_cubes(Qdata,Udata,correction):
+    """
+    Applies the ionospheric Faraday rotation correction to the Stokes Q/U
+    data, derotating the polarization angle and renormalizing to remove
+    depolarization. Note that this will amplify the noise present in the data,
+    particularly if the depolarization is large (|corr| is small).
+    Note that the 'correction' variable is the effect of the ionosphere 
+    (Theta, in the derivation), so the correction process is to divide by this 
+    variable to produce the output cubes.
+    """
+    
+    Pdata=Qdata+1.j*Udata #Input complex polarization
+    Pcorr=Pdata/correction 
+    Qcorr=Pcorr.real
+    Ucorr=Pcorr.imag
+    
+    return Qcorr,Ucorr
+
+
+def apply_correction_to_files(Qfile,Ufile,correctionfile,Qoutfile,Uoutfile,overwrite=False):
+    """
+    This function combines all the individual steps needed to apply a 
+    correction to a set of Q and U FITS cubes and save the results.
+    The user should supply the paths to all the files as specified.
+    The output files can probably(?) be the same as the input files, if you
+    want to overwrite the existing files.
+    """
+    
+    #Get all data:
+    correction=read_correction(correctionfile)
+    Qdata,Udata,header=readData(Qfile,Ufile)
+    
+    #Checks for data consistency.
+    if (Qdata.shape != Udata.shape):
+        raise Exception("Q and U files don't have same dimensions.")
+    if Qdata.shape[0] != correction.size:
+        raise Exception("Correction file does not have same number of channels as FITS cube.")
+
+    #Apply correction
+    Qcorr,Ucorr=correct_cubes(Qdata,Udata,correction)
+    
+    #Save results
+    write_corrected_cubes(Qoutfile,Uoutfile,Qcorr,Ucorr,header,overwrite=overwrite)
+
+
+
+
+def main():
+    """
+    When invoked from the command line, parse the input options to get the
+    filenames and other parameters, then invoke apply_correction_to_files
+    to run all the steps and save the output cubes.
+    """
+    
+    import argparse
+    import os
+    #TODO
+    descStr = """
+    Apply correction for ionospheric Faraday rotation to Stokes Q and U FITS
+    cubes. Requires the file names for the input cubes, output cubes, and the
+    correction file (which contains the ionospheric effect per channel 
+    in the cubes).
+    """
+
+    parser = argparse.ArgumentParser(description=descStr,
+                                 formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("fitsQ",dest="fitsQ",
+                        help="FITS cube containing (uncorrected) Stokes Q data.")
+    parser.add_argument("fitsU",dest="fitsU",
+                        help="FITS cube containing (uncorrected) Stokes U data.")
+    parser.add_argument("correctionFile",dest="correctionFile",
+                        help="File containing ionospheric correction to be applied.")
+    parser.add_argument("Qcorrected",dest="outQ",
+                        help="Output filename for corrected Stokes Q cube.")
+    parser.add_argument("Ucorrected",dest="outU",
+                        help="Output filename for corrected Stokes U cube.")
+
+    parser.add_argument("-o",dest="overwrite",action="store_true",
+                        help="Overwrite exising output files? [False]")
+
+    args = parser.parse_args()
+
+    #Check for file existence.
+    if not os.path.isfile(args.fitsQ):
+        raise Exception("Stokes Q file not found.")
+    if not os.path.isfile(args.fitsU):
+        raise Exception("Stokes U file not found.")
+    
+    #Pass file names into do-everything function.
+    apply_correction_to_files(args.fitsQ,args.fitsU,args.correctionFile,
+                              args.outQ,args.outU,overwrite=args.overwrite)
 
 
 
@@ -34,6 +193,7 @@ def write_corrected_cubes(Qoutputname,Qdata,Udata,header):
 
 
 
-
+if __name__ == "__main__":
+    main()
 
 
--- predict.py
+++ predict.py
@@ -14,33 +14,89 @@ change in polarization angle, and the effective depolarization.
 import RMextract.getRM as RME
 from astropy.time import Time
 import numpy as np
-from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.coordinates import EarthLocation
 import astropy.units as u
 
+C = 2.997924538e8 # Speed of light [m/s]
 
-def get_telescope_coordinates(telescope_name):
+
+
+def get_telescope_coordinates(telescope):
     """Return the astropy.coordinates EarthLocation object associated
-    with the position of the telescope named. Each telescope must be 
-    hardcoded in.
+    with the position of the telescope.
+    The input must be either a string with the name of a pre-programmed telescope,
+     a 3-component tuple with the latitude [deg], longitude [deg], and height [m],
+     or an EarthLocation object.
     """
-    if telescope_name == 'ASKAP':
-        lat = -1*26+42/60+15/3600 #degree
-        long = +1*116+39/60+32/3600 # degree
-        height = 381.0 #
+    if type(telescope) == EarthLocation: #Pass EarthLocations through without processing
+        return telescope
+    elif type(telescope) == str: #Hardcoded coordinates for some telescopes.
+        if telescope == 'ASKAP':
+            lat = -1*26+42/60+15/3600 #degree
+            long = +1*116+39/60+32/3600 # degree
+            height = 381.0 #
+        else:
+            raise Exception("Telescope name not recognized.")
         return EarthLocation(lat=lat*u.deg, lon=long*u.deg, height=height*u.m)
-    else:
-        raise Exception('Telescope position not known.')
-
-
-def integrate_single_timestep(time1,time2,RM1,RM2):
-    pass
-    return ""
+    elif (type(telescope) == tuple) or (type(telescope) == list):
+        return EarthLocation(lat=telescope[0]*u.deg, lon=telescope[1]*u.deg, 
+                             height=telescope[2]*u.m)
 
 
 
-def write_correction(correction_dictionary,filename):
-    pass
 
+
+
+def write_correction(freq_array,corr,filename):
+    np.savetxt(filename, list(zip(freq_array,corr.real,corr.imag)))
+
+
+def numeric_integration(times,RMs,freq_array):
+    """Numerical integration of the time-varying ionospheric polariation 
+    modulation. Testing has shown that numerical integration is accurate 
+    to better than 1% accuracy except where depolarization is extreme (>99%).
+    """
+
+    from scipy.integrate import simps
+    l2_arr=(C/freq_array)**2
+    z=np.exp(2.j*np.outer(l2_arr,RMs))
+    
+    #Scipy's numerical integrators can't handle complex numbers, so the
+    # integral needs to be broken into real and complex components.
+    real=simps(z.real,times,axis=1)
+    imag=simps(z.imag,times,axis=1)
+    corr=(real+1.j*imag)/(times[-1]-times[0])
+    return corr
+    
+
+def check_numeric_problems(RMs, freq_array,corr):
+    """Checks for conditions that might cause numeric instability in the
+    correction, and warns the user if there might be concerns.
+    """
+    import warnings
+    
+    #Check for large jumps in RM/polarization angle between steps.
+    #These can cause the numeric integrator to not catch angle wraps.
+    longest_l2=(C/np.min(freq_array))**2
+    max_deltaRM=np.max(np.diff(RMs))
+    max_delta_polangle=longest_l2*max_deltaRM  #in radians
+    if max_delta_polangle > 0.5:
+        warnings.warn(("\nLarge variations in RM between points, which may "
+                       "introduce numerical errors.\n"
+                       "Consider trying a smaller timestep."))
+    
+    #Warn about very low values of the correction (very strong depolarization)
+    # as these can probably not be corrected reliably.
+    if np.min(np.abs(corr)) < 0.02:
+        warnings.warn(("\nExtreme depolarization predicted (>98%). "
+                       "Corrected polarization will almost certainly not "
+                       "be trustworthy in affected channels."))
+    elif np.min(np.abs(corr)) < 0.1:
+        warnings.warn(("\nSignificant depolarization predicted (>90%). "
+                       "Errors in corrected polarization are likely to be "
+                       "very large in some channels."))
+    
+    
 
 
 def calculate_correction(start_time, end_time, freq_array, telescope_location,
@@ -60,12 +116,15 @@ def calculate_correction(start_time, end_time, freq_array, telescope_location,
         ra (float): right ascension of observation center (in deg, J2000)
         dec (float): declination of observation center (in deg, J2000)
         timestep (float): time in seconds between ionosphere FR estimates
-    Returns: dictionary containing information about correction, containing:
-        ???
+    Returns: times (array): vector of times (in MJD seconds) of each 
+                            ionospheric RM calculation
+             RMs (array): vector of RM values computed for each time step
+             correction (array): vector containing the (complex) ionospheric
+                polarization (Theta) for each frequency channel.
     """
 
     #If necessary, convert telescope name into telescope location object:    
-    if type(telescope_location) == str:
+    if type(telescope_location) != EarthLocation:
         telescope_location=get_telescope_coordinates(telescope_location)
 
     #RMextract wants time ranges in MJD seconds:
@@ -86,6 +145,40 @@ def calculate_correction(start_time, end_time, freq_array, telescope_location,
     #  RM, times, timestep, station_names, stat_pos, flags, reference_time
     times=predictions['times']
     RMs=np.squeeze(predictions['RM']['st1'])
+    
+    #Compute the time-integrated correction. 
+    correction=numeric_integration(times,RMs,freq_array)
+    
+    return times,RMs, correction
+    
+    
+
+def generate_plots(times,RMs,corr,freq_array,savename=None):
+    """Makes a figure with two plots: the RM variation over time, 
+    and the (modulus of the) correction as a function of frequency.
+    If savename contains a string it will save the plots to that filename,
+    otherwise the plots are not saved.
+    """
+    from matplotlib import pyplot as plt
+    from matplotlib import dates as mdates
+    plot_times=Time(times/86400.0,format='mjd').plot_date
+    
+    
+    fig,(ax1,ax2)=plt.subplots(2,1,figsize=(8,8))
+    ax1.plot_date(plot_times,RMs,fmt='k.')
+    locator=mdates.AutoDateLocator(minticks=3,maxticks=7)
+    formatter = mdates.ConciseDateFormatter(locator)
+    ax1.xaxis.set_major_locator(locator)
+    ax1.xaxis.set_major_formatter(formatter)
+    ax1.set_ylabel('$\phi_\mathrm{ion}$ [rad m$^{-2}$]')
+    
+    ax2.plot(freq_array,np.abs(corr),'k.')
+    ax2.set_xlabel('Frequency [Hz]')
+    ax2.set_ylabel('|$\Theta(\lambda^2)$|')
+    
+    if savename is not None:
+        plt.savefig(savename,bbox_inches='tight')
+
 
 
 def main():
@@ -93,10 +186,97 @@ def main():
     and save the predictions, as a function of frequency, to a text file.
     """
     import argparse
-    
-    
+    descStr = """
+    Calculate ionospheric Faraday rotation and predict time-integrated effect
+    as a function of frequency.
+    Can determine the frequency and observation time parameters from a 
+    supplied FITS cube, if it has the correct keywords, 
+    otherwise from those parameters must be supplied on the command line.
+    """
+
+    parser = argparse.ArgumentParser(description=descStr,
+                                 formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-F",dest="fits",default=None,metavar='FILENAME',
+                        help="FITS cube containing frequencies and observation time.")
+    parser.add_argument("-d", dest="times", nargs=2,type=str,default=None,
+                        metavar=('START','END'),
+                        help="start and end time strings.")
+    parser.add_argument("-c", dest=("freq_parms"),nargs=3,default=None,
+                        metavar=('MINFREQ','MAXFREQ','CHANNELWIDTH'),
+                        help=("Generate channel frequencies (in Hz): \n"
+                              "    minfreq, maxfreq, channel_width"))
+    parser.add_argument("-t",dest='telescope_name',type=str,default=None,
+                        help="Telescope name")
+    parser.add_argument("-T",dest='telescope_coords',nargs=3,type=float,default=None,
+                        metavar=('LAT','LONG','ALT'),
+                        help="Telescope coordinates:\n    lat[deg],long[deg], altitude[m].")
+    parser.add_argument('-p',dest='pointing',nargs=2,type=float,default=None,
+                        metavar=('RA','DEC'),
+                        help="Pointing center: RA[deg], Dec[deg]")
+    parser.add_argument("-s", dest='savefile',type=str,default=None,metavar='FILENAME',
+                        help="Filename to save correction data to.")
+    parser.add_argument("-S", dest='savefig',type=str,default=None,metavar='FILENAME',
+                        help="Filename to save the plots to.")
+    parser.add_argument("--timestep",dest='timestep',default=600.,type=float,
+                        help="Timestep for ionospheric prediction, in seconds. Default = 600")
+    args = parser.parse_args()
 
 
+    start_time=None
+    end_time=None
+    freq_arr=None
+    telescope=None
+    ra=None
+    dec=None
+    
+
+    #If a FITS file is present, try to fill in any missing keywords.
+    #But since FITS headers can be very different, it may be that not all
+    #keywords can be found.
+    if args.fits is not None:
+        import astropy.io.fits as pf
+        header=pf.getheader(args.fits)
+        #TO-DO
+    
+    #Any parametrs taken from FITS header can be overridden by manual inputs:
+    if args.times is not None:
+        start_time=Time(args.times[0])
+        end_time=Time(args.times[1])
+        
+    if freq_arr is not None:
+        freq_arr=np.arange(args.freq_parms[0],args.freq_parms[1],args.freq_parms[2])
+
+    if args.telescope is not None:
+        telescope = get_telescope_coordinates(args.telescope)
+    if args.telescope_coords is not None:
+        telescope = get_telescope_coordinates(args.telescope_coords)
+    
+    if args.pointing is not None:
+        ra=args.pointing[0]
+        dec=args.pointing[1]
+    
+    #Check that all parameters are set:
+    missing_parms=[]
+    if (start_time is None): missing_parms.append('Start time')
+    if (end_time is None): missing_parms.append('End time')
+    if (freq_arr is None): missing_parms.append('Frequency array')
+    if (telescope is None): missing_parms.append('Telescope')
+    if (ra is None): missing_parms.append('Pointing center')
+
+    if len(missing_parms) > 0:
+        print("Missing parameters:",missing_parms)
+    
+    times,RMs,correction=calculate_correction(start_time, end_time, freq_arr, telescope,
+                         ra,dec, timestep=args.timestep,ionexPath='./IONEXdata/')
+    
+    if args.savefile is not None:
+        write_correction(freq_arr,correction,args.savefile)
+
+
+    generate_plots(times,RMs,correction,freq_arr,savename=args.savefig)
+    
+    
+    
 
 if __name__ == "__main__":
     main()
